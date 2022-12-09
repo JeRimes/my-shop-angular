66f1ae68c03ec77f7cd11efcb52ffcd5435b5455
JeRimes <lav.jeremy22@gmail.com>
2022-12-09 16:28:08 +0100

    add total

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -6,9 +6,27 @@ import { products } from '../../model/products'
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
+
 export class CartComponent implements OnInit {
 
   cart: Cart[] = [];
+
+  totalPrice = 0;
+
+  addToTotal(number: number) {
+    this.totalPrice += number;
+  }
+  minusToTotal(number: number) {
+    this.totalPrice -= number;
+  }
+  getTotal() {
+    return this.totalPrice;
+  }
+  setTotal(number: number) {
+    this.totalPrice = number;
+  }
+
   ngOnInit() {
     //get local storage ids product
     var cartLocalStorage = this.getCart();
@@ -17,28 +35,27 @@ export class CartComponent implements OnInit {
       cartLocalStorage.forEach((element: string) => {
         products.map((product) => {
           if (product.id.toString() == element) {
+            //set by default the quantity to 1
             var cart = { product: product, quantity: 1 }
             this.cart.push(cart)
+            //add to total price
+            this.addToTotal(product.specifications.price);
           }
         })
       });
-
     }
-    console.log(this.cart);
   }
-
+  //get from local storage : cart
   getCart() {
     var productCart: any[] = [];
     var producStore = localStorage.getItem('productCart');
     if (producStore != null) {
       var cartParse = JSON.parse(producStore);
-      //avoid duplicate for local storage
       cartParse.forEach((element: string) => {
         productCart.push(JSON.parse(element))
 
       });
     }
-    console.log(productCart);
     return productCart;
   }
 
@@ -46,31 +63,35 @@ export class CartComponent implements OnInit {
     this.cart.forEach(element => {
       if (element.product.id == id) {
         element.quantity += 1;
+        this.addToTotal(element.product.specifications.price);
       }
     })
   }
   minusQuantity(id: number) {
-    this.cart.forEach(element => {
+    var cartList = this.cart;
+    cartList.forEach(element => {
+      console.log(cartList.length);
       if (element.product.id == id) {
+        console.log("minus" + element.product.specifications.price);
+
         if (element.quantity > 1) {
           element.quantity -= 1;
         }
         else {
           this.removeProductFromCart(id)
         }
+        this.minusToTotal(element.product.specifications.price)
       }
     })
   }
   removeProductFromCart(id: number) {
     //remove from local storage
     var producStore = localStorage.getItem('productCart');
-    console.log(producStore);
     if (producStore != null) {
       var cartParse = JSON.parse(producStore);
       cartParse = cartParse.filter(function (value: number) {
         return value != id;
       })
-      console.log(cartParse);
       //set the new list of ids
       localStorage.setItem('productCart', JSON.stringify(cartParse))
     }
@@ -79,10 +100,18 @@ export class CartComponent implements OnInit {
     this.cart = this.cart.filter(function (cart) {
       return cart.product.id != id;
     })
+
+
   }
 
   clearCart() {
+    //clear local storage
     localStorage.clear();
+
+    //clear cart
     this.cart.splice(0, this.cart.length);
+
+    //set total to 0
+    this.setTotal(0);
   }
 }
