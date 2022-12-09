067c4f9c4910c9aefdef9f08ffecf852515a9b23
JeRimes <lav.jeremy22@gmail.com>
2022-12-09 10:43:08 +0100

    add product component, local storage and image

---
+++ src/app/component/product/product.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { products } from '../../model/products'
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  products = products;
+
+  ngOnInit() {
+
+  }
+
+  addToCart(id: number) {
+    var productCart = [];
+    var producStore = localStorage.getItem('productCart');
+    if (producStore != null) {
+      var cartParse = JSON.parse(producStore);
+      //avoid duplicate for local storage
+      cartParse.forEach((element: string) => {
+        if (element != id.toString()) {
+          productCart.push(JSON.parse(element))
+        }
+      });
+    }
+    productCart.push(id)
+    localStorage.setItem('productCart', JSON.stringify(productCart))
+    console.log(localStorage.getItem('productCart'));
+  }
+
+  clearCart() {
+    localStorage.clear();
+    console.log(localStorage.getItem('productCart'));
+    console.log("clear local storage !");
+  }
+}
---
+++ src/app/model/products.ts
@@ -0,0 +1,59 @@
+import Product from './types/Product';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "iPhone 12",
+    specifications: {
+      color: "black",
+      weight: "250g",
+      storage: "256GB",
+      price: "1000$"
+    },
+    picture: "/assets/images/iphone12.png"
+  },
+  {
+    id: 2,
+    name: "Airpods",
+    specifications: {
+      color: "white",
+      weight: "50g",
+      storage: "N/A",
+      price: "200$"
+    },
+    picture: "/assets/images/airpods.png"
+  },
+  {
+    id: 3,
+    name: "Samsung S22",
+    specifications: {
+      color: "black",
+      weight: "250g",
+      storage: "512GB",
+      price: "900$"
+    },
+    picture: "/assets/images/samsung22.png"
+  },
+  {
+    id: 4,
+    name: "Samsung Note10",
+    specifications: {
+      color: "black",
+      weight: "250g",
+      storage: "256GB",
+      price: "950$"
+    },
+    picture: "/assets/images/samsungnote10.png"
+  },
+  {
+    id: 5,
+    name: "Phone 14",
+    specifications: {
+      color: "white",
+      weight: "250g",
+      storage: "256GB",
+      price: "1300$"
+    },
+    picture: "/assets/images/iphone14.png"
+  }
+]
